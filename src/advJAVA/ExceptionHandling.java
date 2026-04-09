51efbf2ad1f3a87af361f91c497102195dcc39d0
A SHIVA KUMAR <shivuas605@gmail.com>
2026-04-09 19:41:42 +0530

    multiple catch block

--- src/advJAVA/ExceptionHandling.java
+++ src/advJAVA/ExceptionHandling.java
@@ -4,26 +4,40 @@ public class ExceptionHandling {
 
 	public static void main(String[] args) {
 		
-		int i = 0;
+		int i = 2;
 		int j = 0;
 
-//here we use try and catch block to handle this arithmetic	runtime error.
-//in try block it will try to execute if it throws an error catch block will be executed by excepting an error by object from try block
-//if try block does not get an error it will ignore execution of catch block.catch block execute only when try throws an error.
-		
-		try
-		{
-			j=18/i;
-		}
-		catch(Exception e)
-		{
-			System.out.println("there is some error");
-		}
-		
-		System.out.println(j);
-	
-		
-		System.out.println("hello");
+        int[] nums = new int[5];
+        
+        String s = null;
+        
+        try
+        {
+        	j=18/i;
+        	System.out.println(s.length());
+        	System.out.println(nums[5]);
+        }
+ //multiple catch block can be writen to handle different types exception.  
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+        	System.out.println("your exceding ur limit");
+        }
+        catch(ArithmeticException e)
+        {
+        	System.out.println("your dividing by zero");
+        }
+        
+    //Exception is an parent class where it can handle all types of exception and always it should be bottom.
+        
+        catch(Exception e)
+        {
+        	System.out.println("something went wrong" +  e);
+        }
+        
+        
+        System.out.println(j);
+        System.out.print("hello");
+        
 	}
 
 }
