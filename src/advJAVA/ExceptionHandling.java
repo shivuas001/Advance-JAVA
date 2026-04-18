a19b221193ecb95a6441363ef63a4efc3dec67dd
A SHIVA KUMAR <shivuas605@gmail.com>
2026-04-18 21:47:50 +0530

    Throw keyword in java

--- src/advJAVA/ExceptionHandling.java
+++ src/advJAVA/ExceptionHandling.java
@@ -4,37 +4,29 @@ public class ExceptionHandling {
 
 	public static void main(String[] args) {
 		
-		int i = 2;
+		int i = 20;
 		int j = 0;
-
-        int[] nums = new int[5];
-        
-        String s = null;
         
         try
         {
         	j=18/i;
-        	System.out.println(s.length());
-        	System.out.println(nums[5]);
+       //here we are trying to create an exception by using throw keyword.
+       //here 18/20 will gives zero and catch block does not execute so we only create exception by throw keyword.
+        	if(j==0)
+        	throw new ArithmeticException();
         }
- //multiple catch block can be writen to handle different types exception.  
-        catch(ArrayIndexOutOfBoundsException e)
-        {
-        	System.out.println("your exceding ur limit");
-        }
+
         catch(ArithmeticException e)
         {
-        	System.out.println("your dividing by zero");
+        	System.out.println("your dividing by zero" + e);
         }
         
-    //Exception is an parent class where it can handle all types of exception and always it should be bottom.
         
         catch(Exception e)
         {
         	System.out.println("something went wrong" +  e);
         }
-        
-        
+         
         System.out.println(j);
         System.out.print("hello");
         
