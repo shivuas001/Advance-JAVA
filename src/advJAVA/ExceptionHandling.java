c67952d72214cd997d4d0521770d989d05f8b13b
A SHIVA KUMAR <shivuas605@gmail.com>
2026-05-01 19:02:13 +0530

    Ducking Exception using Throws keyword

--- src/advJAVA/ExceptionHandling.java
+++ src/advJAVA/ExceptionHandling.java
@@ -1,45 +1,27 @@
 package advJAVA;
+//Ducking the exception that means the method which as defined will not handle exception it throws and class who is calling will handle it.
 
-
-//To create our own custom Exception we create a class of it and constructor and class should extend the parent Exception class
-//string because from custom Exception we sending a string message so we call super class to handle.
-
-class ShivaKumarException extends Exception
-{
-	public ShivaKumarException(String string)
-	{
-		super(string);
-	}
+class A{
+	 static void show() throws ClassNotFoundException {
+		 Class.forName("Demo");
+	 }
 }
 
 public class ExceptionHandling {
+	
+	static {
+		System.out.print("class loaded");
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		A obj = new A();
 		
-		int i = 20;
-		int j = 0;
-        
-        try
-        {
-        	j=18/i;
-        	if(j==0)
-        	throw new ShivaKumarException("i dont want to print Zero");
-        }
-
-        catch(ShivaKumarException e)
-        {
-        	System.out.println("your dividing by zero" + e);
-        }
-        
-        
-        catch(Exception e)
-        {
-        	System.out.println("something went wrong" +  e);
-        }
-         
-        System.out.println(j);
-        System.out.print("hello");
-        
+		try {
+			obj.show();
+		} catch (ClassNotFoundException e) {
+			System.out.println("ur class not found" + e);
+		}
+		
 	}
 
 }
